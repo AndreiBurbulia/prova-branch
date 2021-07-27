150457046c32acda7a859cc83bf7aa71f3609965
andreagavardi <andreagavardi89@gmail.com>
2021-07-27 17:30:33 +0200

    add h3

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Hello</h1>
+    <h3>Post Test</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil architecto officia, saepe totam expedita aspernatur debitis consequuntur deleniti doloribus rerum odio amet nesciunt numquam voluptas sed corporis inventore tempore laudantium!</p>
+</body>
+
+</html>
